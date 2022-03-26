b518baae0111558734102723301b46821dea1522
fernandacornejom <fernandacornejom@gmail.com>
2022-03-26 11:05:37 -0300

    finished

---
+++ lorem_generator.rb
@@ -0,0 +1,6 @@
+num = ARGV[0].to_i
+
+num.times do 
+    puts "Is he keeping his shoulders square? Oooooooh he's tryin'! Grassss... Taste bad! Well, she's my daughter, Summer. I outrank you. Or, family means nothing, in which case, don't play that card. I'm your worst nightmare, bitch!"
+end
+
